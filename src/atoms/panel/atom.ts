adac2a0ae60f6126f03ad712d13c6d9a5bcadd70
Jeongeun-Choi <cje3080@gmail.com>
2025-07-02 23:00:23 +0900

    feat: 패널 선택 후 캔버스 영역 클릭시 도형이 나타나도록 구현

---
+++ src/atoms/panel/atom.ts
@@ -0,0 +1,6 @@
+import { atom } from "jotai";
+
+export const selectedPanelAtom = atom("");
+export const setSelectedPanelAtom = atom(null, (_, set, update: string) =>
+  set(selectedPanelAtom, update)
+);
