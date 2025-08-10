f87b15e18ab3f1d4f8aa457c1b4a7c72e6f1c6ec
jeongeun <cje3080@gmail.com>
2025-08-10 16:05:43 +0900

    chore(code): 불필요한 기능 제거 (#11)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { useSetAtom } from "jotai";
-import { useState, type MouseEvent, type PropsWithChildren } from "react";
+import { type MouseEvent, type PropsWithChildren } from "react";
 import GNB from "../features/gnb/components/GNB";
 import "./MainLayout.css";
 import Panel from "../features/panel/components/Panel";
@@ -11,12 +11,7 @@ const componentList = [
 ];
 
 export default function MainLayout({ children }: PropsWithChildren) {
-  const [foldingLeftPanel, setFoldingLeftPanel] = useState(false);
   const setSelectedPanel = useSetAtom(setSelectedPanelAtom);
-
-  const handleFoldingPanel = () => {
-    setFoldingLeftPanel((prev) => !prev);
-  };
 
   const handleClickButton = (event: MouseEvent<HTMLButtonElement>) => {
     setSelectedPanel(event.currentTarget.dataset.type || "");
@@ -26,7 +21,7 @@ export default function MainLayout({ children }: PropsWithChildren) {
     <div>
       <GNB />
       <section style={{ height: "100vh" }}>
-        {!foldingLeftPanel && (
+        {
           <Panel>
             {componentList.map((component) => (
               <li>
@@ -36,16 +31,7 @@ export default function MainLayout({ children }: PropsWithChildren) {
               </li>
             ))}
           </Panel>
-        )}
-        <button
-          id="folding-left-btn"
-          onClick={handleFoldingPanel}
-          className={"folding-left-btn ".concat(
-            foldingLeftPanel ? "folding" : ""
-          )}
-        >
-          {foldingLeftPanel ? "열어" : "접어"}
-        </button>
+        }
         {children}
       </section>
     </div>
